a5030bd173c8634c6265ec31c22fd653914e0579
hihumikan <musecharo@gmail.com>
2021-09-03 06:26:42 +0900

    4

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import * as React from 'react'
 import './App.css'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>
+        <h2>Dogアプリ</h2>
+      </header>
+      <p>犬の画像を表示するサイトです</p>
+      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" />
     </div>
   )
 }
