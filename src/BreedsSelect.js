79e19bacf4165d4862a17eaf62d28d5c204288d0
hihumikan <musecharo@gmail.com>
2021-09-04 19:03:11 +0900

    10

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,1 +1,15 @@
-// DO NOT DELETE
+import React from 'react'
+
+export function BreedsSelect( breeds, selectedBreed, setSelectedBreed ) {
+  return (
+    <div>
+      <select  onChange={setSelectedBreed}>
+        {breeds.breeds.map((breed, i) => (
+          <option key={i} value={breed}>
+            {breed}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react'
+import { BreedsSelect } from './BreedsSelect'
 
 export function DogListContainer() {
   useEffect(() => {
     fetch('https://dog.ceo/api/breeds/list/all')
       .then(response => response.json())
       .then(data => {
-        setBreeds(data.message)
+        setBreeds(Object.keys(data.message))
       })
   }, [])
   const [breeds, setBreeds] = useState([])
-
-  return <p>test</p>
+  const [selectedBreed, setSelectedBreed] = useState('none')
+  const handleClick = event => {
+    setSelectedBreed(event.target.value)
+  }
+  return (
+    <div>
+      <BreedsSelect
+        breeds={breeds}
+        selectedBreed={selectedBreed}
+        setSelectedBreed={handleClick}
+      />
+      <p>{selectedBreed}</p>
+    </div>
+  )
 }
